8cb19684218b0b975524f93b7e9f56154f7f783d
ke yongyu <247321453@qq.com>
2016-04-25 11:20:59 +0800

    Update registerurl.cmd

--- registerurl.cmd
+++ registerurl.cmd
@@ -1,4 +1,4 @@
-set CUR_DIR="%CD%"
+set CUR_DIR=%CD%
 REG ADD HKCR\upyun /f /ve /d "URL:UpYun Protocol"
 REG ADD HKCR\upyun /f /v "URL Protocol" /d ""
 REG ADD HKCR\upyun\DefaultIcon /f /ve /d """%CUR_DIR%\UpYunManager.exe"",2"
